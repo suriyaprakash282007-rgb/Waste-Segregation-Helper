76538dc3b0e580efa8ef1e1fc1f7033ccf943098
Suriya Prakash <suriyaprakash282007@gmail.com>
2026-03-16 21:20:51 +0530

    Move layout.jsx to app/layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+// Updated layout for app directory
+
+export default function Layout({ children }) {
+    return (
+        <html>
+            <head />
+            <body>{children}</body>
+        </html>
+    );
+}
